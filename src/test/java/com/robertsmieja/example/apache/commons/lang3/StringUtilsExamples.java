6e7d12adb4ab4b1bc2b609739420f4a28d29b3d9
Robert Smieja <robertsmieja@gmail.com>
2017-03-22 03:23:08 -0400

    Add more StringUtil examples

--- src/test/java/com/robertsmieja/example/apache/commons/lang3/StringUtilsExamples.java
+++ src/test/java/com/robertsmieja/example/apache/commons/lang3/StringUtilsExamples.java
@@ -7,9 +7,11 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -18,35 +20,74 @@ import static org.junit.Assert.assertEquals;
 public class StringUtilsExamples {
 
     @Test
+    public void isBlankExamples(){
+        assertFunction(StringUtils::isBlank, "", true);
+        assertFunction(StringUtils::isBlank, null, true);
+        assertFunction(StringUtils::isBlank, " ", true);
+        assertFunction(StringUtils::isBlank, " This isn't blank ", false);
+    }
+
+    @Test
+    public void isEmptyExamples(){
+        assertFunction(StringUtils::isEmpty, "", true);
+        assertFunction(StringUtils::isEmpty, null, true);
+        assertFunction(StringUtils::isEmpty, " ", false);
+        assertFunction(StringUtils::isEmpty, " This isn't blank ", false);
+    }
+
+    @Test
     public void capitalizationExamples() {
-        //This is a list that contains a Pair of 'expected', 'actual' results
-        List<Pair<String, String>> listOfStringPairs = Arrays.asList(
-                new ImmutablePair<>("", ""),
-                new ImmutablePair<>(" ", " "),
-                new ImmutablePair<>("Foo", "foo"),
-                new ImmutablePair<>("Foo bar", "foo bar"),
-                new ImmutablePair<>("1234", "1234")
-        );
-        //This flips the right and left values so we can test a reverse operation
-        List<Pair<String, String>> flippedListOfStringPairs = flipListOfPairs(listOfStringPairs);
+        assertFunction(StringUtils::capitalize, "", "");
+        assertFunction(StringUtils::capitalize, " ", " ");
+        assertFunction(StringUtils::capitalize, "foo", "Foo");
+        assertFunction(StringUtils::capitalize, "foo bar", "Foo bar");
+        assertFunction(StringUtils::capitalize, "1234", "1234");
+    }
 
-        assertFunctionMatchesList(listOfStringPairs, StringUtils::capitalize);
-        assertFunctionMatchesList(flippedListOfStringPairs, StringUtils::uncapitalize);
+    @Test
+    public void uncapitalizationExamples() {
+        assertFunction(StringUtils::uncapitalize, "", "");
+        assertFunction(StringUtils::uncapitalize, " ", " ");
+        assertFunction(StringUtils::uncapitalize, "Foo", "foo");
+        assertFunction(StringUtils::uncapitalize, "Foo bar", "foo bar");
+        assertFunction(StringUtils::uncapitalize, "1234", "1234");
+    }
+
+    @Test
+    public void splitExamples(){
+        assertFunction(StringUtils::split, "Here are a bunch of words.", new String[]{"Here", "are", "a", "bunch", "of", "words."});
+        assertFunction(StringUtils::split, "C, S, V, example",",", new String[]{"C", " S", " V", " example"});
+    }
+
+    @Test
+    public void differenceExamples(){
+        assertFunction(StringUtils::difference, "Similar string", "Similar string kindof", " kindof");
+        assertFunction(StringUtils::difference, "same string", "same string", "");
+        assertFunction(StringUtils::difference, "different string", "foo bar", "foo bar");
     }
 
 
-    private void assertFunctionMatchesList(List<Pair<String, String>> pairList, Function<String, String> function) {
-        pairList.stream().forEach((pair) -> assertFunctionMatchesPair(pair, function));
-    }
 
-    private void assertFunctionMatchesPair(Pair<String, String> expectedAndInputPair, Function<String, String> function) {
-        assertEquals(expectedAndInputPair.getLeft(), function.apply(expectedAndInputPair.getRight()));
-    }
 
-    private <T> List<Pair<T, T>> flipListOfPairs(List<Pair<T, T>> listOfPairs) {
-        return
-                listOfPairs.stream()
-                        .map(pair -> new ImmutablePair<>(pair.getRight(), pair.getLeft()))
-                        .collect(Collectors.toList());
+
+
+
+
+
+
+    /* Helpers */
+    private <T, R> void assertFunction(Function<T, R> function, T input, R output){
+        if (output instanceof Object[]){
+            assertArrayEquals((R[])output, (R[]) function.apply(input));
+        } else {
+            assertEquals(output, function.apply(input));
+        }
+    }
+    private <T, O, R> void assertFunction(BiFunction<T, O, R> function, T input, O otherInput, R output){
+        if (output instanceof Object[]){
+            assertArrayEquals((R[])output, (R[]) function.apply(input, otherInput));
+        } else {
+            assertEquals(output, function.apply(input,otherInput));
+        }
     }
 }
