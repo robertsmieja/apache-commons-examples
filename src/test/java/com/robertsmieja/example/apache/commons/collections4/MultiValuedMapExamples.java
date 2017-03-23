4c0c57651fae0e8a9c2ad2b17c04d419127ea0ab
Robert Smieja <robertsmieja@gmail.com>
2017-03-22 23:26:33 -0400

    Add MultiValuedMapExamples

---
+++ src/test/java/com/robertsmieja/example/apache/commons/collections4/MultiValuedMapExamples.java
@@ -0,0 +1,62 @@
+package com.robertsmieja.example.apache.commons.collections4;
+
+import org.apache.commons.collections4.MultiValuedMap;
+import org.apache.commons.collections4.multimap.HashSetValuedHashMap;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.*;
+
+/**
+ * Example of multi-value map
+ */
+public class MultiValuedMapExamples {
+    MultiValuedMap<Integer, String> multiMap;
+
+    @Before
+    public void setup() {
+        multiMap = new HashSetValuedHashMap<>();
+    }
+
+    @Test
+    public void basicMapOperations() {
+        assertEquals(0, multiMap.size());
+
+        multiMap.put(1, "foo");
+        assertEquals(1, multiMap.size());
+
+        Collection<String> strings = multiMap.get(1);
+        assertFalse(strings.isEmpty());
+        assertEquals(1, strings.size());
+
+        multiMap.remove(1);
+        assertEquals(0, multiMap.size());
+    }
+
+    @Test
+    public void addingMultipleValues() {
+        multiMap.put(1, "foo");
+        assertEquals(1, multiMap.size());
+
+        multiMap.put(1, "bar");
+        assertEquals(2, multiMap.size());
+
+        Collection<String> strings = multiMap.get(1);
+        assertEquals(2, strings.size());
+        assertThat(strings, containsInAnyOrder("foo", "bar"));
+    }
+
+    @Test
+    public void addingAListWithAnAdd(){
+        multiMap.putAll(1, Arrays.asList("foo", "bar"));
+        assertEquals(2, multiMap.size());
+
+        Collection<String> strings = multiMap.get(1);
+        assertEquals(2, strings.size());
+        assertThat(strings, containsInAnyOrder("foo", "bar"));
+    }
+}
