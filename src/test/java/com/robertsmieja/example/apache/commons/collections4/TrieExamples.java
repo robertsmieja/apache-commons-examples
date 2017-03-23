62c6d4d89801caaf130afdde6ecd89b7fd82019e
Robert Smieja <robertsmieja@gmail.com>
2017-03-23 00:18:50 -0400

    Update TrieExamples

--- src/test/java/com/robertsmieja/example/apache/commons/collections4/TrieExamples.java
+++ src/test/java/com/robertsmieja/example/apache/commons/collections4/TrieExamples.java
@@ -8,11 +8,18 @@ import org.junit.Test;
 import java.util.Map;
 import java.util.SortedMap;
 
+/**
+ * Examples of Trie, which is a type of "search tree".
+ * It's an ordered map where each "node" has prefixes based on the key.
+ * <p>
+ * A PATRICIA Trie is the only read/write implementation included with Apache Commons,
+ * and is a "compact prefix tree" since it stores information in nodes other than the leaves.
+ */
 public class TrieExamples {
     Trie<String, String> trie;
 
     @Before
-    public void setup(){
+    public void setup() {
         trie = new PatriciaTrie<>();
 
         trie.put("Awesome", "value");
@@ -24,15 +31,17 @@ public class TrieExamples {
     }
 
     @Test
-    public void triePrefixMap(){
+    public void triePrefixMap() {
         SortedMap<String, String> prefixMap = trie.prefixMap("A");
         printPrefixMap(prefixMap);
+
+        System.out.println();
 
         prefixMap = trie.prefixMap("Awe");
         printPrefixMap(prefixMap);
     }
 
-    private void printPrefixMap(SortedMap<String, String> prefixMap){
+    private void printPrefixMap(SortedMap<String, String> prefixMap) {
         System.out.println("-------");
         for (Map.Entry<String, String> entry : prefixMap.entrySet()) {
             System.out.println("Key: <" + entry.getKey() + ">, Value: <" + entry.getValue() + ">");
